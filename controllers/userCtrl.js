b8dd08f5456e1bfbe02280a225d384fec95eb9c9
BaayXaliil <ibrahima.diop@idev-ic.net>
2022-03-23 00:16:41 +0000

    OK

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -33,7 +33,7 @@ const userCtrl = {
             const url = `${CLIENT_URL}/home/overview/${activation_token}`
             sendMail(email, url, "Verify your email address");
             
-            res.json({msg: "Register Success! Please activate your email to start."})
+            res.json({msg: "Register Success! Please activate your email to start.", url})
 
         } catch (err) {
             return res.status(500).json({msg: err.message})
