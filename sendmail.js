f9ddba986a7efd8c0f55a1365fe4c675bc4837dd
BaayXaliil <ibrahima.diop@idev-ic.net>
2022-03-24 19:14:02 +0000

    using Nodemailer in heroku

--- sendmail.js
+++ sendmail.js
@@ -16,7 +16,7 @@ var send = function(email, url, text) {
         subject: 'Envoie de mail en utilisant Node.js',
         html: `
         <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-        <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the DevAT channel.</h2>
+        <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to KHALIL Dev.</h2>
         <p>Congratulations! You're almost set to start using DEVAT✮SHOP.
             Just click the button below to validate your email address.
         </p>
